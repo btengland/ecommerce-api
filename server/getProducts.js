ff47620a1d1716d95083be944ec839a926915a03
Brady England <bradyeng22@gmail.com>
2021-01-28 10:17:48 -0700

    fixed

--- server/getProducts.js
+++ server/getProducts.js
@@ -2,7 +2,7 @@ const products = require('../products.json')
 
 const getProducts = (req, res) => {
     if(req.query.price) {
-        const items = porducts.filter(
+        const items = products.filter(
             (val) => val.price >= parseInt(req.query.price)
         )
         return res.status(200).send(items)
